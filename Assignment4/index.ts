7e4aeaf49f325db52ce20c112bb34a8fd4845ac6
Hamza Islam <starhax4@gmail.com>
2024-04-05 11:54:48 +0500

    solved 3rd problem

--- Assignment4/index.ts
+++ Assignment4/index.ts
@@ -1,2 +1,24 @@
-console.log("Hello World!");
+// 1. Hello World Problem
+let greeting = "Hello World!" //Assigned Hello World! to a variable greeting.
+console.log(greeting); // Printed greeting to the console.
+
+// 2.BAsic Maths
+let num1 , num2; //Declared two variables num1 and num2.
+num1 = 10; //Assigned 10 to num1.
+num2 = 50; //Assigned 20 to num2.
+console.log(num1 + num2); //Printed the sum of num1 and num2.
+console.log(num1 - num2); //Printed the difference of num1 and num2.
+console.log(num1 * num2); //Printed the product of num1 and num2.
+console.log(num1 / num2); //Printed the quotient of num1 and num2.
+
+// 3.Swapping Values
+let a = 1; // Initialized a with value 1
+let b= 2; // Initialized b with value 2
+
+a = a + b; // 1+2 =3
+b=  a - b; // now b = 3 - 2 = 1 that is originally = a
+a = a - b; // now a has value 3 - 1 = 2 that is original value of b
+
+console.log(a);
+console.log(b);
 
